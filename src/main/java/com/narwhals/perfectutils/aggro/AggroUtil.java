f8f298634718c6401cae0c3951c4593f68de8e32
arfemia <andyfemia@gmail.com>
2026-05-28 19:53:40 -0400

    update 5

--- src/main/java/com/narwhals/perfectutils/aggro/AggroUtil.java
+++ src/main/java/com/narwhals/perfectutils/aggro/AggroUtil.java
@@ -11,7 +11,7 @@ import com.hypixel.hytale.component.ComponentType;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.logger.HytaleLogger;
-import com.hypixel.hytale.math.vector.Vector3d;
+import org.joml.Vector3d;
 import com.hypixel.hytale.server.core.asset.type.attitude.Attitude;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
